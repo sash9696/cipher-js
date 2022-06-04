5bf047e3967417895699a999b71474ca8ffad4f6
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-06-04 21:34:26 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,106 @@
+// console.log("Hi there");
+//it just prints out hi there
+/*this is a 
+multiline comment*/
+//this is a
+//multiple comment
+
+// var a;
+// a = 10;
+// var b = "John";
+
+//8 basic data types in javascript
+
+// const message = "hello";
+// message = 12345;
+
+// console.log(message);
+
+//1 Number
+
+// let n = 123;
+// n = 12.345;
+// console.log(n);
+
+//specific numeric numbers
+//Infinity
+// alert(1 / 0);
+// console.log(1 / 0);
+// console.log(Infinity);
+
+//NaN
+
+// console.log(NaN + 1);
+// console.log(NaN * 3);
+// console.log("not a number" / 2 - 1);
+
+// console.log(NaN * 0);
+// console.log(NaN ** 0);  //exception
+
+// //2 BigInt
+// const bigInt = 9007199254740991n;
+
+// console.log(bigInt);
+
+//3 String
+//simple quotes
+// let str = "Hello";
+// let str2 = "Single Quotes";
+
+// //extended functionality embeds a value ${}
+// let str3 = `${str} , how are you doing`;
+
+// console.log(str3);
+
+// console.log(`the result is ${1 + 2}`);
+// console.log("the substraction is ${2 - 1}");
+
+//4 Boolean (logical type)
+
+// let isChecked = false;
+
+// let isGreater = 4 > 1;
+// console.log(isGreater);
+
+//5 Null
+// let age = null;
+// console.log(age);
+//its just a special value which represents nothing, empty or a value unknown
+
+//6 undefined
+//it means value is not defined
+
+//7 Object and Symbols nom-primitive
+//used for more complex data structures
+
+//Type Coversions
+
+// alert(8);
+
+//String Conversion
+
+// let value = true;
+// value = String(value);
+// console.log(typeof value);
+
+//Numeric Conversion
+
+// alert("6" / "2");
+
+// let str = "123";
+
+// let num = Number(str);
+
+// console.log(typeof num);
+
+// let age = Number("A string instead of a number");
+// console.log(age);
+
+// console.log(Number("    123     "));
+
+// console.log(Number(true));
+// console.log(Number("123a"));
+
+// console.log(Number(undefined));
+
+// console.log(Number(null));
